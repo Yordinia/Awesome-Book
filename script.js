3bdb29354162462a3e3e9c79d7a749d893e3fc9f
Yordi <yoriyordina7@gmail.com>
2022-12-12 17:06:59 +0300

    update style

--- script.js
+++ script.js
@@ -3,8 +3,24 @@ const title = document.querySelector(".title");
 const author = document.querySelector(".author");
 const booksList = document.querySelector(".books");
 
+//form.preventDefault();
+
+
 const books = [];
 
-function addBook(){
-    const book = {title: ''}
+
+function callback(e){
+    e.preventDefault();
+
+    const book = {
+        title: title.value,
+        author: author.value
+    }
+//localStorage.setItem('book-value', JSON.stringify(book))
+    
+    console.log('created book',book)
 }
+
+
+button.addEventListener('click', callback)
+
