fe37c1ce5b080997a36e9c75024a16e2309cade6
Yordi <yoriyordina7@gmail.com>
2022-12-12 16:40:19 +0300

    commit changes

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(".Button");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const booksList = document.querySelector(".books");
+
+const books = [];
+
+function addBook(){
+    const book = {title: ''}
+}
