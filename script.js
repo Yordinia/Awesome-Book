c2432e47684c84b9b9990f776d5c05c4f552192a
Kamran123Ahmad <kamrankhan.ahmad2@yahoo.com>
2022-12-12 23:18:41 -0800

    Dynamically Add remove button

--- script.js
+++ script.js
@@ -3,45 +3,49 @@ const title = document.querySelector(".title");
 const author = document.querySelector(".author");
 const booksList = document.querySelector(".books");
 
+const books = JSON.parse(localStorage.getItem("books-list")) || [];
 
-
-const books = JSON.parse(localStorage.getItem('books-list')) || [];
-
-function remove(i){
-    //remove that button
-   
+function remove(i) {
+  let x = document.querySelectorAll(".book")[i];
+  booksList.removeChild(x);
+  books.splice(i);
+  localStorage.setItem("books-list", JSON.stringify(books));
+  display();
 }
 
-function display(){
-    if(books.length === 0) {booksList.innerText = 'No books in local storage'; return 0; }
-let i =0;
-  booksList.innerHTML = ''
-  if(books.length!==0){
-     books.forEach(book =>{
-    booksList.innerHTML +=  `<div class='book'>
-    title ${book.title} by <em> ${book.author}  </em><button onclick="remove(${i++})"> Remove </button> </div>
-    `
-    })
+function display() {
+  if (books.length === 0) {
+    booksList.innerText = "No books in local storage";
+    return 0;
+  }
+  let i = 0;
+  booksList.innerHTML = "";
+  if (books.length !== 0) {
+    books.forEach((book) => {
+      booksList.innerHTML += `<div class='book'>
+    <strong>Title:</strong> ${book.title} <br />
+    <strong>Author:</strong> ${book.author} 
+    <br/><button onclick="remove(${i++})"> Remove </button> </div> <br /><hr>
+    `;
+    });
   }
 }
-display()
-function callback(e){
-    e.preventDefault();
+display();
+function callback(e) {
+  e.preventDefault();
 
-    const book = {
-        title: title.value,
-        author: author.value
-    }
+  const book = {
+    title: title.value,
+    author: author.value,
+  };
 
-  books.push(book)
-  localStorage.setItem('books-list', JSON.stringify(books))
-   
+  books.push(book);
+  localStorage.setItem("books-list", JSON.stringify(books));
+
   display();
 
-  title.value = ''
-  author.value = ''
+  title.value = "";
+  author.value = "";
 }
 
-
-button.addEventListener('click', callback)
-
+button.addEventListener("click", callback);
