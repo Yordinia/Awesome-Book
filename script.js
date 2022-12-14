3d29fb5326976ff0d118f8dd2c44a235c2755440
Kamran123Ahmad <kamrankhan.ahmad2@yahoo.com>
2022-12-14 06:49:37 -0800

    fix linter errors

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 /* eslint-disable no-unused-vars */
 
-const button = document.querySelector(".Button");
-const title = document.querySelector(".title");
-const author = document.querySelector(".author");
-const booksList = document.querySelector(".books");
+const button = document.querySelector('.Button');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const booksList = document.querySelector('.books');
 
-const books = JSON.parse(localStorage.getItem("books-list")) || [];
+const books = JSON.parse(localStorage.getItem('books-list')) || [];
 
 class Books {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
+
   static callback(e) {
     e.preventDefault();
 
     const book = new Books(title.value, author.value);
 
     books.push(book);
-    localStorage.setItem("books-list", JSON.stringify(books));
+    localStorage.setItem('books-list', JSON.stringify(books));
 
     Books.display();
 
-    title.value = "";
-    author.value = "";
+    title.value = '';
+    author.value = '';
   }
 
   static display() {
     if (books.length === 0) {
-      booksList.innerText = "No books in local storage";
+      booksList.innerText = 'No books in local storage';
       return 0;
     }
     let i = 0;
-    booksList.innerHTML = "";
+    booksList.innerHTML = '';
     if (books.length !== 0) {
       books.forEach((book) => {
         booksList.innerHTML += `<div class='book'>
@@ -45,14 +46,15 @@ class Books {
     }
     return 0;
   }
+
   static remove(i) {
-    const x = document.querySelectorAll(".book")[i];
+    const x = document.querySelectorAll('.book')[i];
     booksList.removeChild(x);
     books.splice(i, 1);
-    localStorage.setItem("books-list", JSON.stringify(books));
+    localStorage.setItem('books-list', JSON.stringify(books));
     Books.display();
   }
 }
 
 Books.display();
-button.addEventListener("click", Books.callback);
+button.addEventListener('click', Books.callback);
