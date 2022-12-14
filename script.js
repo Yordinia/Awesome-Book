c6810df8986b3552a9750d2c86241678ad81b3c8
Kamran123Ahmad <kamrankhan.ahmad2@yahoo.com>
2022-12-13 20:18:56 -0800

    implemented methods using classes and provided styles

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
 /* eslint-disable no-unused-vars */
 
-const button = document.querySelector('.Button');
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
-const booksList = document.querySelector('.books');
+const button = document.querySelector(".Button");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const booksList = document.querySelector(".books");
+
+const books = JSON.parse(localStorage.getItem("books-list")) || [];
 
 class Books {
-  constructor (title,author){
-  this.title = title;
-  this.author = author;
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-}
+  static callback(e) {
+    e.preventDefault();
 
-const books = JSON.parse(localStorage.getItem('books-list')) || [];
+    const book = new Books(title.value, author.value);
 
-function display() {
-  if (books.length === 0) {
-    booksList.innerText = 'No books in local storage';
+    books.push(book);
+    localStorage.setItem("books-list", JSON.stringify(books));
+
+    Books.display();
+
+    title.value = "";
+    author.value = "";
+  }
+
+  static display() {
+    if (books.length === 0) {
+      booksList.innerText = "No books in local storage";
+      return 0;
+    }
+    let i = 0;
+    booksList.innerHTML = "";
+    if (books.length !== 0) {
+      books.forEach((book) => {
+        booksList.innerHTML += `<div class='book'>
+      <strong>Title:</strong> ${book.title} <br />
+      <strong>Author:</strong> ${book.author} 
+      <br/><button onclick="Books.remove(${i})"> Remove </button> </div> <br /><hr>
+      `;
+        i += 1;
+      });
+    }
     return 0;
   }
-  let i = 0;
-  booksList.innerHTML = '';
-  if (books.length !== 0) {
-    books.forEach((book) => {
-      booksList.innerHTML += `<div class='book'>
-    <strong>Title:</strong> ${book.title} <br />
-    <strong>Author:</strong> ${book.author} 
-    <br/><button onclick="remove(${i})"> Remove </button> </div> <br /><hr>
-    `;
-      i += 1;
-    });
+  static remove(i) {
+    const x = document.querySelectorAll(".book")[i];
+    booksList.removeChild(x);
+    books.splice(i, 1);
+    localStorage.setItem("books-list", JSON.stringify(books));
+    Books.display();
   }
-  return 0;
 }
 
-function remove(i) {
-  const x = document.querySelectorAll('.book')[i];
-  booksList.removeChild(x);
-  books.splice(i, 1);
-  localStorage.setItem('books-list', JSON.stringify(books));
-  display();
-}
-
-display();
-function callback(e) {
-  e.preventDefault();
-
-const book = new Books(title.value,author.value);
-
-  books.push(book);
-  localStorage.setItem('books-list', JSON.stringify(books));
-
-  display();
-
-  title.value = '';
-  author.value = '';
-}
-
-button.addEventListener('click', callback);
+Books.display();
+button.addEventListener("click", Books.callback);
